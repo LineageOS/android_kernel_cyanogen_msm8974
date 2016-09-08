e4f535ab0404427200e26710aaa3dcbe0db16e56
fonz93 <fonzino993@gmail.com>
2016-09-08 08:03:54 +0200

    sound: msm8974: fix buttons of my crappy samsung headsets
    Change-Id: I3e2e1ebcb5d2fd528344935026974642ec90a06d

--- sound/soc/msm/msm8974.c
+++ sound/soc/msm/msm8974.c
@@ -1791,7 +1791,7 @@ void *def_taiko_mbhc_cal(void)
 	btn_low[6] = 229;
 	btn_high[6] = 269;
 	btn_low[7] = 270;
-	btn_high[7] = 500;
+	btn_high[7] = 680;
 	n_ready = wcd9xxx_mbhc_cal_btn_det_mp(btn_cfg, MBHC_BTN_DET_N_READY);
 	n_ready[0] = 80;
 	n_ready[1] = 68;
